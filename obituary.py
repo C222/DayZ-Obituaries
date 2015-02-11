013f289642cb0647870dab8cde059995f5062305
C222 <c222streams@gmail.com>
2015-02-10 16:13:49 -0800

    Made Visualizer class.

--- obituary.py
+++ obituary.py
@@ -1,11 +1,21 @@
 import regex_parse
+import graphviz
+
+class LogViz(object):
+    def __init__(self, playerdict, everyone = False, engine = 'dot'):
+        self.graph = graphviz.Digraph(comment='Vizualization', format='png', engine=engine)
+        for p in playerdict:
+            if playerdict[p].kills or playerdict[p].deaths or everyone:
+                self.graph.node(p, playerdict[p].name)
+                for k in playerdict[p].kills:
+                    self.graph.edge(p, k.id)
 
 class Player(object):
     def __init__(self, name, id):
         self.name = name
         self.id = id
-        self.connected = None
-        self.disconnected = None
+        self.connected = []
+        self.disconnected = []
         self.kills = []
         self.deaths = []
         
@@ -55,18 +65,23 @@ def main():
             
             if connect_line.group(3) not in players:
                 players[connect_line.group(3)] = Player(connect_line.group(2),connect_line.group(3))
-                
+            
+            players[connect_line.group(3)].connected += [connect_line.group(1)]
+            
             connect_line = None
             
         if disconnect_line:
             
             if disconnect_line.group(3) not in players:
                 players[disconnect_line.group(3)] = Player(disconnect_line.group(2),disconnect_line.group(3))
-                
+            
+            players[disconnect_line.group(3)].disconnected += [disconnect_line.group(1)]
+            
             disconnect_line = None
             
-    for p in players:
-        print players[p].dict()
+    lv = LogViz(players, True, engine='fdp')
+    print lv.graph.source
+    print lv.graph.render()
 
 if __name__ == "__main__":
     main()
